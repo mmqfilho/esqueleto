0751f16804b4204d4745d3b56913fea32abdc899
Marcos Menezes <mmqfilho@users.noreply.github.com>
2020-03-11 11:27:17 -0300

    Create 404.php

---
+++ app/view/erro/404.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>404 - Endereço não encontrado</title>
+</head>
+<style>
+    body {font-family: Arial;text-align: center;}
+    h1 {font-size: 150px;margin-top: 100px; margin-bottom: 0px;}
+    h2 { font-size: 24px;margin-top: 0px;}
+</style>
+<body>
+    <h1>404</h1>
+    <h2>Endereço não Encontrado</h2>
+</body>
+</html>
