75334dfb25dab276ee82ff0f69bdab52b7611753
Marcos Menezes <mmqfilho@users.noreply.github.com>
2020-03-11 11:27:39 -0300

    Create view404.php

---
+++ app/view/erro/view404.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>404 - View não encontrada</title>
+</head>
+<style>
+    body {font-family: Arial;text-align: center;}
+    h1 {font-size: 150px;margin-top: 100px; margin-bottom: 0px;}
+    h2 { font-size: 24px;margin-top: 0px;}
+</style>
+<body>
+    <h1>404</h1>
+    <h2>View não Encontrada</h2>
+</body>
+</html>
